471bb1492c7159b43b69c7fd3ae533fff76ed566
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-05-27 23:54:05 -0700

    first commit

---
+++ lib/constants/translation.dart
@@ -0,0 +1,14 @@
+abstract class Translation {
+  static Map<String, Map<String, String>> translationsKeys = {
+    "en": en,
+    "fa": fa,
+  };
+}
+
+final Map<String, String> en = {
+  //
+};
+
+final Map<String, String> fa = {
+  //
+};
