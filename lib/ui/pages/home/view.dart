306a6e61346f44f4facf16561a77de16a9a5751d
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-05-29 04:10:10 -0700

    first commit

--- lib/ui/pages/home/view.dart
+++ lib/ui/pages/home/view.dart
@@ -1,9 +1,13 @@
 import 'package:easypayamak/constants/config.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:smooth_widgets/widgets/appbar/appbar.dart';
+import 'package:smooth_widgets/widgets/buttons/icon.button.dart';
+import 'package:smooth_widgets/widgets/skeletons/button.listtile.skeleton.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -25,6 +29,12 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   void dispose() {
     _tabController.dispose();
     super.dispose();
+  }
+
+  void refresh() {
+    setState(() {
+      //
+    });
   }
 
   @override
@@ -69,12 +79,146 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
         physics: BouncingScrollPhysics(),
         dragStartBehavior: DragStartBehavior.down,
         children: [
-          Center(
-            child: Text("sent".tr.capitalizeFirst!),
+          FutureBuilder(
+            future: null,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active ||
+                  snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.data != null) {
+                  return Center(
+                    child: Text("followings".tr.capitalizeFirst!),
+                  );
+                } else {
+                  return Center(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/svg/not_found.svg",
+                            color: !context.isDarkMode
+                                ? Colors.black
+                                : Colors.white,
+                            width: 250,
+                          ),
+                          const SizedBox(height: 25),
+                          Text(
+                            "هنوز هیچ کسی اینجا نیست!",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: !context.isDarkMode
+                                  ? Colors.black
+                                  : Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return SmoothButtonListTileSkeleton();
+                  },
+                );
+              } else if (snapshot.connectionState == ConnectionState.none) {
+                return Center(
+                  child: SmoothIconButton(
+                    icon: Ionicons.reload,
+                    onPressed: () {
+                      refresh();
+                    },
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return SmoothButtonListTileSkeleton();
+                  },
+                );
+              }
+            },
           ),
-          Center(
-            child: Text("received".tr.capitalizeFirst!),
-          )
+          FutureBuilder(
+            future: null,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active ||
+                  snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.data != null) {
+                  return Center(
+                    child: Text("followings".tr.capitalizeFirst!),
+                  );
+                } else {
+                  return Center(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/svg/not_found.svg",
+                            color: !context.isDarkMode
+                                ? Colors.black
+                                : Colors.white,
+                            width: 250,
+                          ),
+                          const SizedBox(height: 25),
+                          Text(
+                            "هنوز هیچ کسی اینجا نیست!",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: !context.isDarkMode
+                                  ? Colors.black
+                                  : Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return SmoothButtonListTileSkeleton();
+                  },
+                );
+              } else if (snapshot.connectionState == ConnectionState.none) {
+                return Center(
+                  child: SmoothIconButton(
+                    icon: Ionicons.reload,
+                    onPressed: () {
+                      refresh();
+                    },
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return SmoothButtonListTileSkeleton();
+                  },
+                );
+              }
+            },
+          ),
         ],
       ),
     );
